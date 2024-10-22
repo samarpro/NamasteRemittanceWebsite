3467227e3ba3fe230581ff82c4a2be8d7e82229c
samarpro <samarpangupta123@gmail.com>
2024-10-22 14:08:11 +1100

    bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
       </div>
       <nav className='flex items-center '>
         <ul className={`absolute flex flex-col lg:flex-row left-0 top-0 m-1 gap-4 h-full w-full bg-red-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 lg:static lg:bg-transparent lg:backdrop-filter-none lg:backdrop-blur-none lg:flex border-none justify-start items-center text-xl lg:text-base font-jakarta font-extrabold lg:font-medium opacity-0 lg:opacity-100 ${navState ? 'left-0 opacity-100' : '-left-full'} transition-all duration-500 `}>
-          <RxCross2 className='lg:hidden' style={{ marginLeft: 'auto', fontSize: '40px', marginRight: '10px', marginTop: '10px' }} onClick={() => setNavState(false)} />
+          <RxCross2 className='lg:hidden z-50' style={{ marginLeft: 'auto', fontSize: '40px', marginRight: '10px', marginTop: '10px' }} onClick={() => setNavState(false)} />
           <li>
             <Link to="upcoming/">Home</Link>
           </li>
@@ -33,7 +33,7 @@ function Navbar() {
       <div className="navigators flex items-center gap-x-3 md:gap-x-3">
         <Button text="Login" className={'border rounded-md p-1 px-2 md:px-3 border-black '} link='https://namasteremittance.com.au/admin/login.php' />
         <Button text="Sign Up" className={'rounded-md p-2 px-5 text-white bg-gradient-to-l from-red-600 to-red-700 transition-all duration-[1000ms] ease-in-out hover:shadow-[0px_0px_10px_1px_rgba(255,0,51,1)] border hover:border hover:border-red-300'} link='https://namasteremittance.com.au/admin/register.php' />
-        <GiHamburgerMenu className="lg:hidden z-50" style={{ fontSize: '30px' }} onClick={()=>setNavState(true)} />
+        <GiHamburgerMenu className="lg:hidden z-40" style={{ fontSize: '30px' }} onClick={()=>setNavState(true)} />
       </div>
     </header>
   )
